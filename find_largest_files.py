c8970cec0f33e2cae472845721890f79e1ad7aa3
Adrian Steinert <asteinert@egnyte.com>
2021-05-07 18:14:19 +0200

    find largest files on the system

---
+++ find_largest_files.py
@@ -0,0 +1,31 @@
+import time
+import os
+
+from os.path import join, getsize   # getsize is a generator function 
+from heapq import nlargest
+
+def walk_files_and_sizes(start_at: str):
+    for root, _, files in os.walk(start_at):
+        for file in files:
+            path = join(root, file)
+            try:
+                size = getsize(path)    # returns size in bytes
+                yield path, size
+            except OSError:
+                continue 
+
+
+def largest_files(n: int, start_at: str) -> None:
+    MB = 1024 * 1024
+    largest = nlargest(n, walk_files_and_sizes(start_at), key=lambda x: x[1])
+
+    for path, size in largest:
+        print(f'{size // MB} MB, {path}')
+
+
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    largest_files(10, "/Users/asteinert")
+    elapsed = time.perf_counter() - start
+    print(f'{elapsed} seconds elapsed')
